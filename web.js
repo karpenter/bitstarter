59f3fc75942c539ae553eca0919df3b9a3f401eb
Panos Karapanos <panos.kar@gmail.com>
2013-07-06 02:56:50 -0700

    Made changes to web.js to read from index.html

--- web.js
+++ web.js
@@ -2,17 +2,11 @@ var express = require('express');
 var fs = require('fs');
 var index = new Buffer(256);
 index = fs.readFileSync("index.html");
-var index_to_str = index.toString();
-// console.log(index_to_str);
-
-
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  //response.send('Hello World 2!');
   response.send(index.toString());
-    
 });
 
 var port = process.env.PORT || 5000;
