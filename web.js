bd7516af47ba14de86ca96268254cc68d7e9f21e
Panos Karapanos <panos.kar@gmail.com>
2013-07-06 01:58:20 -0700

    Made changes to web.js to read from index.html

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
  // response.send('Hello World 2!');
-  response.send(out);
+  response.send(index);
     
 });
 
